e609a8093a839163797217c2a734657c8a29c7ab
Saurabh Jadhav <saurabh20official@gmail.com>
2026-06-08 23:44:15 +0530

    Added BCrypt password encryption for user registration

--- src/main/java/com/agrohub/service/UserService.java
+++ src/main/java/com/agrohub/service/UserService.java
@@ -1,6 +1,7 @@
 package com.agrohub.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.agrohub.entity.User;
@@ -12,7 +13,14 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     public User registerUser(User user) {
+
+        user.setPassword(
+                passwordEncoder.encode(user.getPassword())
+        );
 
         return userRepository.save(user);
     }
